a1cf7e991c7aa760d96f65136e67c63d898d02cd
trample <trample68@163.com>
2013-03-25 16:55:13 -0400

    fix minor problems

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-
 # encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
@@ -34,4 +33,3 @@ ActiveRecord::Schema.define(:version => 20130321221842) do
   end
 
 end
-
